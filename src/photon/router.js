f6cc3fb728fcc904ab9e9b453fccd7a3128ec363
Andy McCoy <andy.k.mccoy@gmail.com>
2022-02-07 02:12:04 -0800

    remove logging

--- src/photon/router.js
+++ src/photon/router.js
@@ -4,27 +4,19 @@ const photonClient = require('./client');
 const logger = require('../lib/logger');
 
 router.get('/geocode', async (req, res) => {
-  logger.info('triggered /geocode');
   try {
     const resp = await photonClient.request({
       method: 'get',
       url: '/api',
       params: req.query
     });
-    logger.info('/geocode successful call to photon');
     res.json(resp.data);
   } catch (error) {
-    logger.info('error');
     if (error.response) {
-      if (error.response.status > 499) {
-        logger.error(error);
-      }
       res.sendStatus(error.response.status);
       res.send(error.response.data);
     }
     else {
-      logger.info('some weird error');
-      logger.error(error);
       res.sendStatus(500);
     }
   }
@@ -32,27 +24,19 @@ router.get('/geocode', async (req, res) => {
 });
 
 router.get('/reverse', async (req, res) => {
-  logger.info('triggered /reverse');
   try {
     const resp = await photonClient.request({
       method: 'get',
       url: '/reverse',
       params: req.query
     });
-    logger.info('/reverse successful call to photon');
     res.json(resp.data);
   } catch (error) {
-    logger.info('error');
     if (error.response) {
-      if (error.response.status > 499) {
-        logger.error(error);
-      }
       res.sendStatus(error.response.status);
       res.send(error.response.data);
     }
     else {
-      logger.info('some weird error');
-      logger.error(error);
       res.sendStatus(500);
     }
   }
