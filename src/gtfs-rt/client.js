893f624a241c814fcf777ce2c3ce484b37598b88
Scott Feeney <scott@oceanbase.org>
2023-03-11 16:57:11 -0800

    support env vars defined locally for GTFS RT config

--- src/gtfs-rt/client.js
+++ src/gtfs-rt/client.js
@@ -1,12 +1,8 @@
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
+import logger from '../lib/logger.js';
 
-// FIXME
-const API_KEY = 'paste api key here';
-const BASE_URL = 'https://api.511.org/transit/servicealerts';
-const PARAMS = {
-  api_key: API_KEY,
-  agency: 'RG',
-};
+const API_KEY = process.env.GTFS_REALTIME_TOKEN;
+const ALERTS_URL = process.env.GTFS_REALTIME_ALERTS_URL;
 
 let _alertsCache;
 let _alertsCacheTime;
@@ -20,14 +16,32 @@ export async function getAlerts() {
     return _alertsCache;
   }
 
+  if (!API_KEY) {
+    logger.warn('no API key defined, cannot fetch alerts');
+    return null;
+  }
+
+  if (!ALERTS_URL) {
+    logger.warn('no GTFS-RT alerts URL defined, cannot fetch alerts');
+    return null;
+  }
+
+  // Add the API key to the URL.
+  // TODO: Figure out how to support API implementations that require it as a request
+  // header instead, if that exists. I don't know. This seems to be a non-standard thing
+  // that 511.org does, requiring it in the URL.
+  const url = new URL(ALERTS_URL);
+  const usp = new URLSearchParams(url.search);
+  usp.append('api_key', API_KEY);
+  url.search = usp;
+
   // FIXME: This will result in possibly sending additional requests while waiting
   // for the first one.
-  const url = new URL(`${BASE_URL}?${new URLSearchParams(PARAMS)}`);
   const response = await fetch(url);
   if (!response.ok) {
     const err = new Error(`${response.url}: ${response.status} ${response.statusText}`);
     err.response = response;
-    throw error;
+    throw err;
   }
   const buf = await response.arrayBuffer();
   const alertFeed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
