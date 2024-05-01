a694a325a7e16c9d01ddcbbbaa0b5528058b4235
Andy McCoy <andy.k.mccoy@gmail.com>
2024-05-01 15:25:06 -0700

    Add trip updates endpoint

--- src/realtime-gtfs/router.js
+++ src/realtime-gtfs/router.js
@@ -4,16 +4,12 @@ import realtimeClient from './client.js';
 import cache from '../lib/cache.js';
 import logger from '../lib/logger.js';
 import {
-  GTFS_REALTIME_ALERTS_URL,
   GTFS_REALTIME_VEHICLE_POSITIONS_URL,
   GTFS_REALTIME_TRIP_UPDATES_URL,
 } from '../config.js';
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
 
-// const root = await protobuf.load('src/proto/gtfs-realtime.proto');
-
 const vehiclePositionsUrl = GTFS_REALTIME_VEHICLE_POSITIONS_URL;
-const serviceAlertsUrl = GTFS_REALTIME_ALERTS_URL;
 const tripUpdatesUrl = GTFS_REALTIME_TRIP_UPDATES_URL;
 
 const router = express.Router();
@@ -21,19 +17,21 @@ const router = express.Router();
 const validateQueryParams = () => checkSchema({
   'trip-id': {
     optional: true,
+    trim: true,
     isLength: {
       options: { 
         min: 3,
-        max: 32
+        max: 64
       },
     },
   },
   'route-id': {
     optional: true,
+    trim: true,
     isLength: {
       options: { 
         min: 3,
-        max: 32
+        max: 64
       },
     },
   }
@@ -62,20 +60,25 @@ function filterVehiclePositions(tripId, routeId, entities) {
 }
 
 function filterTripUpdates(tripId, routeId, entities) {
-  const vehicleFilters = [{
-    key: 'TripId',
+  const filters = [{
+    key: 'tripId',
     value: tripId
   },
   {
-    key: 'RouteId',
+    key: 'routeId',
     value: routeId
-  }].filter(vehicleFilter => vehicleFilter.value)
+  }].filter(filter => filter.value?.length);
 
-  return vehicleFilters.reduce((entities, filter) => {
+  if (filters.length === 0) return entities;
+
+  return filters.flatMap(filter => {
     return entities
-      .filter(entity => entity.TripUpdate.Trip)
-      .filter(entity => entity.TripUpdate.Trip[filter.key] === filter.value);
-  }, entities);
+      .filter(entity => {
+        if (!entity.tripUpdate) return false;
+        if (!entity.tripUpdate.trip) return false;
+        return filter.value.includes(entity.tripUpdate.trip[filter.key]);
+      });
+  });
 }
 
 // ttl in ms
@@ -179,66 +182,6 @@ async function vehiclePositionsCb (req, res) {
   res.end();
 }
 
-async function serviceAlertsCb (req, res) {
-  if (!serviceAlertsUrl) {
-    logger.info('env var GTFS_REALTIME_ALERTS_URL not found');
-    res.sendStatus(404);
-    res.end();
-    return;
-  }
-
-  // try to get data from cache
-  try {
-    const cacheResult = await cache.get('serviceAlerts', {raw: true});
-    if (cacheResult) {
-      res.header({
-        'X-Cache-Hit': true,
-        'Cache-Control': 'public, max-age=60',
-        'Age': Math.floor((cacheResult.expires - Math.floor(new Date().getTime())) / 1000)
-      });
-
-      res.send(cacheResult.value);
-      res.end()
-      return;
-    }
-  } catch (error) {
-    logger.error(`cache error: ${error}`);
-  }
-  
-  // set cache header to false
-  res.header('X-Cache-Hit', 'false');
-  
-  // try to get data from realtime gtfs upstream
-  try {
-    const {data: serviceAlerts} = await realtimeClient.request({
-      method: 'get',
-      url: serviceAlertsUrl
-    });
-
-    // cache result from upstream
-    try {
-      await cache.set('serviceAlerts', serviceAlerts, 60000); // cache for 60 seconds
-    } catch (error) {
-      logger.error(`cache error: ${error}`);
-    }
-    
-    // set cache headers and send result
-    res.header({
-      'Cache-Control': 'public, max-age=60',
-      'Age': 0
-    });
-    res.send(serviceAlerts);
-  } catch (error) {
-    if (error.response) {
-      res.sendStatus(error.response.status);
-    }
-    else {
-      res.sendStatus(500);
-    }
-  }
-  res.end();
-}
-
 async function tripUpdatesCb (req, res) {
   if (!tripUpdatesUrl) {
     logger.info('env var GTFS_REALTIME_TRIP_UPDATES_URL not found');
@@ -247,60 +190,70 @@ async function tripUpdatesCb (req, res) {
     return;
   }
 
-  // try to get data from cache
-  try {
-    const cacheResult = await cache.get('tripUpdates', {raw: true});
-    if (cacheResult) {
-      res.header({
-        'X-Cache-Hit': true,
-        'Cache-Control': 'public, max-age=60',
-        'Age': Math.floor((cacheResult.expires - Math.floor(new Date().getTime())) / 1000)
-      });
-
-      res.send(cacheResult.value);
-      res.end();
-      return;
-    }
-  } catch (error) {
-    logger.error(`cache error: ${error}`);
+  const validatationResult = validationResult(req);
+  if (!validatationResult.isEmpty()) {
+    res.header('Cache-Control', 'no-store');
+    res.status(400);
+    res.json({ errors: validatationResult.array() });
+    return;
   }
-  
-  // set cache header to false
-  res.header('X-Cache-Hit', 'false');
-  
-  // try to get data from realtime gtfs upstream
-  try {
-    const {data: tripUpdates} = await realtimeClient.request({
-      method: 'get',
-      url: tripUpdatesUrl
-    });
 
-    // cache result from upstream
-    try {
-      await cache.set('tripUpdates', tripUpdates, 60000); // cache for 60 seconds
-    } catch (error) {
-      logger.error(`cache error: ${error}`);
-    }
-    
-    // set cache headers and send result
-    res.header({
-      'Cache-Control': 'public, max-age=60',
-      'Age': 0
-    });
-    res.send(tripUpdates);
+  res.header('Cache-Control', 'max-age=60, public');
+  
+  // get data from realtime gtfs upstream
+  let tripUpdates;
+  try {
+    tripUpdates = await cacheableRequest(60000, tripUpdatesUrl);
   } catch (error) {
+    logger.error(`error fetching trip updates ${error}`);
+    res.header('Cache-Control', 'no-store');
     if (error.response) {
       res.sendStatus(error.response.status);
     }
     else {
       res.sendStatus(500);
     }
+    return;
+  }
+
+  // decode protobuffer
+  let tripUpdatesAll;
+  try {
+    tripUpdatesAll = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+      Buffer.from(tripUpdates.value)
+    );
+  } catch (error) {
+    logger.error(`Error decoding trip updates protobuff: ${error}`);
+    res.header('Cache-Control', 'no-store');
+    res.sendStatus(500);
+    return;
+  }
+
+  // filter undesired trips/routes
+  const tripUpdatesFiltered = {
+    header: tripUpdatesAll.header,
+    entity: filterTripUpdates(req.query['trip-id'], req.query['route-id'], tripUpdatesAll.entity),
+  };
+
+  // send response in correct format
+  res.header('Age', tripUpdates.age);
+  switch(req.accepts(['application/x-protobuf', 'application/json'])) {
+    case 'application/x-protobuf':
+      const encodedProtoBuf = GtfsRealtimeBindings.transit_realtime.FeedMessage.encode(tripUpdatesFiltered).finish();
+      res.header('Content-Type', 'application/x-protobuf');
+      res.send(encodedProtoBuf);
+      break;
+    case 'application/json':
+      res.json(tripUpdatesFiltered);
+      break;
+    default: // 400 must use Accepts header
+      res.sendStatus(400);
+      break;
   }
   res.end();
 }
 
 router.get('/vehiclepositions', validateQueryParams(), vehiclePositionsCb);
-router.get('/servicealerts', serviceAlertsCb);
-router.get('/tripupdates', tripUpdatesCb);
+router.get('/tripupdates', validateQueryParams(), tripUpdatesCb);
 
 export default router;
