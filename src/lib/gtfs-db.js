0ddd391d3681340234222afaeb1c93a825a5397e
Scott Feeney <scott@oceanbase.org>
2025-06-06 20:56:11 -0700

    explicitly pass db to gtfs query func

--- src/lib/gtfs-db.js
+++ src/lib/gtfs-db.js
@@ -17,10 +17,13 @@ export function ensureGtfsDb() {
 // Get the stop's parent station ID, if it has one; if not, the input stop ID is
 // returned.
 export function getParentStationStopId(stopId) {
-  ensureGtfsDb();
-  let stopInfo = getStops({
-    stop_id: stopId,
-  }, ['parent_station'])[0];
+  const db = ensureGtfsDb();
+  let stopInfo = getStops(
+    { stop_id: stopId, },
+    ['parent_station'],
+    [],
+    { db },
+  )[0];
 
   if (!stopInfo || stopInfo.parent_station == null) return stopId;
   return stopInfo.parent_station;
