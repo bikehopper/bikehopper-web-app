8332da0c13c54fd486c1c7f13da19ad87eafff83
Arindam Bose <arindam.1993@gmail.com>
2025-09-27 21:35:37 -0700

    add region_config and elevators from data-mirror

--- coolify/install.sh
+++ coolify/install.sh
@@ -1,6 +1,6 @@
 # download data into input dir
 mkdir -p input
-npx --yes @bikehopper/data-mirror -f gtfs -o input 
+npx --yes @bikehopper/data-mirror -f gtfs,region_config,elevators -o input 
 
 #compile tippecanoe
 apt-get update && apt-get -y install gcc g++ make libsqlite3-dev zlib1g-dev
