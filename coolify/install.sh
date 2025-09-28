79af5dd304b7e918d4fd1ea5342d03229bff2368
Scott Feeney <scott@oceanbase.org>
2025-09-27 18:09:46 -0700

    Revert "move gtfs-process to build step"
    This reverts commit 95fa9d7662d1cb8df0a3f9476e88550aec040de3.
    
    The persistent volume is not available in the Coolify build phase.

--- coolify/install.sh
+++ coolify/install.sh
@@ -1,4 +1,3 @@
 mkdir -p input
 npx --yes @bikehopper/data-mirror -f gtfs -o input # download data
 npm install
-npm run gtfs-process
--- coolify/run.sh
+++ coolify/run.sh
@@ -1,1 +1,1 @@
-node src/index.js
+npm run gtfs-process && node src/index.js
