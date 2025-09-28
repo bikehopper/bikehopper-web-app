c336b8e73732f6e5fd2ab440e5c434de9f35387e
Scott Feeney <scott@oceanbase.org>
2025-09-27 17:53:09 -0700

    remove check-node

--- coolify/install.sh
+++ coolify/install.sh
@@ -1,28 +1,3 @@
-check_node() {
-    if which node > /dev/null
-    then
-        node_version=$(node -v | grep -Eo '[0-9]+' | head -1)
-        if [ $node_version -eq 22 ]
-        then
-            return 1
-        fi
-    fi
-
-    return 0
-}
-
-check_node
-has_node=$?
-# detect and install node version if necessary
-if [ "$has_node" -eq "0" ] 
-then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm install 22
-    nvm use 22
-fi
 mkdir -p input
 npx --yes @bikehopper/data-mirror -f gtfs -o input # download data
 npm install
