1c41eca15ee7d4f8b395434b25c1eaa5449d147d
Scott Feeney <scott@oceanbase.org>
2025-11-09 18:15:59 -0800

    remove ramda
    we don't need this. one less dependency to worry about upgrading

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-import { isEmpty } from 'ramda';
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
 import fs from 'node:fs';
@@ -38,12 +37,12 @@ export const PORT = Number(process.env.PORT) || 3001;
 export const GRAPHHOPPER_SERVICE_NAME = process.env.GRAPHHOPPER_SERVICE_NAME;
 export const PHOTON_SERVICE_NAME = process.env.PHOTON_SERVICE_NAME;
 export const NOMINATIM_SERVICE_NAME = process.env.NOMINATIM_SERVICE_NAME;
-export const CACHE_CONN_STRING = isEmpty(process.env.CACHE_CONN_STRING) ? undefined : process.env.CACHE_CONN_STRING;
+export const CACHE_CONN_STRING = Boolean(process.env.CACHE_CONN_STRING) ? undefined : process.env.CACHE_CONN_STRING;
 
-export const GTFS_REALTIME_TOKEN = isEmpty(process.env.GTFS_REALTIME_TOKEN) ? null : process.env.GTFS_REALTIME_TOKEN;
-export const GTFS_REALTIME_VEHICLE_POSITION_TOKEN = isEmpty(process.env.GTFS_REALTIME_VEHICLE_POSITION_TOKEN) ? null : process.env.GTFS_REALTIME_VEHICLE_POSITION_TOKEN;
-export const GTFS_REALTIME_TRIP_UPDATES_TOKEN = isEmpty(process.env.GTFS_REALTIME_TRIP_UPDATES_TOKEN) ? null : process.env.GTFS_REALTIME_TRIP_UPDATES_TOKEN;
-export const GTFS_REALTIME_SERVICE_ALERTS_TOKEN = isEmpty(process.env.GTFS_REALTIME_SERVICE_ALERTS_TOKEN) ? null : process.env.GTFS_REALTIME_SERVICE_ALERTS_TOKEN;
+export const GTFS_REALTIME_TOKEN = Boolean(process.env.GTFS_REALTIME_TOKEN) ? null : process.env.GTFS_REALTIME_TOKEN;
+export const GTFS_REALTIME_VEHICLE_POSITION_TOKEN = Boolean(process.env.GTFS_REALTIME_VEHICLE_POSITION_TOKEN) ? null : process.env.GTFS_REALTIME_VEHICLE_POSITION_TOKEN;
+export const GTFS_REALTIME_TRIP_UPDATES_TOKEN = Boolean(process.env.GTFS_REALTIME_TRIP_UPDATES_TOKEN) ? null : process.env.GTFS_REALTIME_TRIP_UPDATES_TOKEN;
+export const GTFS_REALTIME_SERVICE_ALERTS_TOKEN = Boolean(process.env.GTFS_REALTIME_SERVICE_ALERTS_TOKEN) ? null : process.env.GTFS_REALTIME_SERVICE_ALERTS_TOKEN;
 
 // 511.org allows us 60 requests per hour, let's be conservative and
 // cache for 3min to make it a maximum of 20.
@@ -52,8 +51,8 @@ export const ALERTS_CACHE_TIME_MSEC = (
   process.env.ALERTS_CACHE_TIME_MSEC || DEFAULT_ALERTS_CACHE_TIME_MSEC
 );
 
-export const MAPBOX_ACCESS_TOKEN = isEmpty(process.env.MAPBOX_ACCESS_TOKEN) ? null : process.env.MAPBOX_ACCESS_TOKEN;
-export const MAPBOX_STYLE_URL = isEmpty(process.env.MAPBOX_STYLE_URL) ? null : process.env.MAPBOX_STYLE_URL;
+export const MAPBOX_ACCESS_TOKEN = Boolean(process.env.MAPBOX_ACCESS_TOKEN) ? null : process.env.MAPBOX_ACCESS_TOKEN;
+export const MAPBOX_STYLE_URL = Boolean(process.env.MAPBOX_STYLE_URL) ? null : process.env.MAPBOX_STYLE_URL;
 
 export const FILTERED_AGENCY_IDS = process.env.FILTERED_AGENCY_IDS || '';
 export const MANUALLY_FILTERED_ROUTE_IDS = process.env.MANUALLY_FILTERED_ROUTE_IDS || '';
