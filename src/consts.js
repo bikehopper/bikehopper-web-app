a426da017d974f0ec7a7a69f7d0c0aa557a927ff
Arindam Bose <arindam.1993@gmail.com>
2026-01-19 17:32:03 -0800

    remove hardcoded data.bikehopper.org reference

--- src/consts.js
+++ src/consts.js
@@ -4,8 +4,8 @@ export const GEO_CONFIG_FOLDER_PATH = 'config/geoconfigs';
 const regionConfig = JSON.parse(fs.readFileSync('data/region-config.json', 'utf8'));
 export const REGION_CONFIG = regionConfig;
 
-export const GTFS_REALTIME_ROOT_URL = process.env.GTFS_REALTIME_ROOT_URL ?? 'https://data.bikehopper.org';
+export const GTFS_REALTIME_ROOT_URL = process.env.GTFS_REALTIME_ROOT_URL;
 
-export const GTFS_REALTIME_ALERTS_URL = `${GTFS_REALTIME_ROOT_URL}/rt/service_alerts.pbf`;
-export const GTFS_REALTIME_VEHICLE_POSITIONS_URL = `${GTFS_REALTIME_ROOT_URL}/rt/vehicle_positions.pbf`;
-export const GTFS_REALTIME_TRIP_UPDATES_URL = `${GTFS_REALTIME_ROOT_URL}/rt/trip_updates.pbf`;
+export const GTFS_REALTIME_ALERTS_URL = GTFS_REALTIME_ROOT_URL ? `${GTFS_REALTIME_ROOT_URL}/rt/service_alerts.pbf` : null;
+export const GTFS_REALTIME_VEHICLE_POSITIONS_URL = GTFS_REALTIME_ROOT_URL ? `${GTFS_REALTIME_ROOT_URL}/rt/vehicle_positions.pbf`: null;
+export const GTFS_REALTIME_TRIP_UPDATES_URL = GTFS_REALTIME_ROOT_URL ? `${GTFS_REALTIME_ROOT_URL}/rt/trip_updates.pbf` : null;
