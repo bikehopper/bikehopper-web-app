32e326be07d5d829c1b0587eb5e6974c4a9ab7d3
Arindam Bose <arindam.1993@gmail.com>
2026-01-19 16:11:35 -0800

    cleanup GTFS RT

--- src/consts.js
+++ src/consts.js
@@ -4,7 +4,8 @@ export const GEO_CONFIG_FOLDER_PATH = 'config/geoconfigs';
 const regionConfig = JSON.parse(fs.readFileSync('data/region-config.json', 'utf8'));
 export const REGION_CONFIG = regionConfig;
 
-export const GTFS_REALTIME_ALERTS_URL = regionConfig.gtfsRtUrls?.alerts;
-export const GTFS_REALTIME_VEHICLE_POSITIONS_URL =
-regionConfig.gtfsRtUrls?.vehiclePositions;
-export const GTFS_REALTIME_TRIP_UPDATES_URL = regionConfig.gtfsRtUrls?.tripUpdates;
+export const GTFS_REALTIME_ROOT_URL = process.env.GTFS_REALTIME_ROOT_URL ?? 'https://data.bikehopper.org';
+
+export const GTFS_REALTIME_ALERTS_URL = `${GTFS_REALTIME_ROOT_URL}/rt/service_alerts.pbf`;
+export const GTFS_REALTIME_VEHICLE_POSITIONS_URL = `${GTFS_REALTIME_ROOT_URL}/rt/vehicle_positions.pbf`;
+export const GTFS_REALTIME_TRIP_UPDATES_URL = `${GTFS_REALTIME_ROOT_URL}/rt/trip_updates.pbf`;
